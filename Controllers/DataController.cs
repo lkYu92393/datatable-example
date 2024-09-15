b5ec7e23662cfc92c9d34846a7e393786e4778de
lkyu <gundam1945@hotmail.com>
2024-09-16 00:56:18 +0800

    first

---
+++ Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataTableTest.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult GetData(Models.DataTable.DataTableParameter dtParams)
+        {
+            List<Models.Employee> employeeList = Models.Employee.GetMockData(200);
+            List<Models.Employee> filterList = employeeList;
+
+            return Json(new
+            {
+                data = filterList,
+                recordsTotal = employeeList.Count,
+                recordsFiltered = filterList.Count,
+            });
+        }
+    }
+}
---
+++ Models/DataTable/DataTableParameter.cs
@@ -0,0 +1,34 @@
+﻿namespace DataTableTest.Models.DataTable
+{
+    public class DataTableParameter
+    {
+        public int draw { get; set; }
+        public List<DataTableColumns> columns { get; set; }
+        public List<DataTableOrder> order { get; set; }
+        public int start { get; set; }
+        public int length { get; set; }
+        public DataTableSearch search { get; set; }
+    }
+
+    public class DataTableColumns
+    {
+        public string data { get; set; }
+        public object name { get; set; }
+        public bool searchable { get; set; }
+        public bool orderable { get; set; }
+        public DataTableSearch search { get; set; }
+    }
+
+    public class DataTableSearch
+    {
+        public string value { get; set; }
+        public string regex { get; set; }
+    }
+
+    public class DataTableOrder
+    {
+        public string column { get; set; }
+        public string dir { get; set; }
+        public string name { get; set; }
+    }
+}
